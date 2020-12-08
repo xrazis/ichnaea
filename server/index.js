0b26d69cef92c5febd1c8aad0943f420783cc683
Haris Razis <xaris.ws98@gmail.com>
2020-12-08 18:24:27 +0200

    ➕ Add bin

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+
+const measurementRoute = require('./routes/data')
+
+app.use(measurementRoute)
+
+module.exports = app;
