79fb7d0e9875f1e4b4e22593c232bcc9cd9755a6
Haris Razis <dev@xrazis.com>
2020-12-16 09:23:21 +0200

    ❌ Remove test lines

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ const app = require('express')()
 const server = require('http').createServer(app);
 const chalk = require('chalk')
 
-const redis = require('./connections/redis_conn')
-const influx = require('./connections/influx_conn')
-const socket = require('./services/socket')(server)
+require('./services/socket')(server);
 
 const measurementRoute = require('./routes/data')
 
