9a09281fe8f48f07246df2737f584df4cb71df77
Haris Razis <dev@xrazis.com>
2020-12-16 09:22:31 +0200

    ➕ Add services

--- server/services/socket.js
+++ server/services/socket.js
@@ -1,5 +1,8 @@
 const socket = require('socket.io');
-const chalk = require('chalk')
+const chalk = require('chalk');
+
+const {pub, sub} = require('../connections/redis_conn')
+const {write, closeWrite, query} = require('../actions/influx_actions')
 
 module.exports = (server) => {
     // noinspection JSValidateTypes
@@ -7,5 +10,16 @@ module.exports = (server) => {
 
     io.on('connection', socket => {
         console.log(chalk.bgWhiteBright.black.bold(`Client with id: ${chalk.bgBlack.whiteBright(socket.id)} just connected!`));
+
+        socket.on('disconnect', () => {
+            console.log(chalk.red('Client disconnected!'));
+        });
+
+        socket.on('data', (data) => {
+            const {measurement, pointName} = data;
+
+            pub.publish('data', measurement);
+            write(socket.id, measurement, pointName)
+        })
     });
 }
