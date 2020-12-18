6219f1ac9d3496a07dcdf0d675aefa14a47112eb
Haris Razis <dev@xrazis.com>
2020-12-18 17:31:53 +0200

    ➕ Add basic config for app.js
    cookie-session / bodyParser / authRoute / passport

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 const app = require('express')()
 const server = require('http').createServer(app);
+const bodyParser = require('body-parser');
+const passport = require('./services/passport');
 const chalk = require('chalk')
-
 require('./services/socket')(server);
 
-const measurementRoute = require('./routes/data')
+const dataRoute = require('./routes/data')
+const authRoute = require('./routes/data')
+const {session_secret} = require('./config/keys');
 
-app.use(measurementRoute)
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-server.listen(6000);
-console.log(chalk.green.bold('Server listening on port 6000!'));
+app.use(
+    require('cookie-session')({
+        keys: [session_secret],
+        maxAge: 30 * 24 * 60 * 60 * 1000
+    })
+);
+
+app.use(passport.initialize(undefined));
+app.use(passport.session(undefined));
+
+app.use(dataRoute)
+app.use(authRoute)
+
+server.listen(6000, () => {
+    console.log(chalk.green.bold('Server listening on port 6000!'));
+});
