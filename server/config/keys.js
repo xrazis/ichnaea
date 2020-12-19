4845f50cc82e3d46fa97bd600e74203b1d7f7d2d
Haris Razis <dev@xrazis.com>
2020-12-19 15:09:13 +0200

    ❌ Remove keys
    No need for redis login, running in docker bridge network

--- server/config/keys.js
+++ server/config/keys.js
@@ -3,8 +3,6 @@ module.exports = {
     influx_url: 'http://influx:8086',
     influx_org: 'xrazis',
     influx_bucket: 'db0',
-    redis_user: '',
-    redis_password: '',
     redis_uri: 'redis://redis:6379',
     mongo_uri: 'mongodb://mongo:27017/user'
 }
