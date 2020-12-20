a97327f4f2cf0cfb07d8602b4045c5af99bc4bb9
Haris Razis <dev@xrazis.com>
2020-12-20 21:11:54 +0200

    ➕ Add cors and /api prefix to auth routes

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ const server = require('http').createServer(app);
 const bodyParser = require('body-parser');
 const passport = require('./services/passport');
 const chalk = require('chalk')
+const cors = require('cors')
 require('./services/socket')(server);
 
 const dataRoute = require('./routes/data')
 const authRoute = require('./routes/data')
 const {session_secret} = require('./config/keys');
+
+//allow for all requests for development.
+app.use(cors())
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -24,7 +28,7 @@ app.use(passport.initialize(undefined));
 app.use(passport.session(undefined));
 
 app.use(dataRoute)
-app.use(authRoute)
+app.use('/api', authRoute)
 
 server.listen(6000, () => {
     console.log(chalk.green.bold('Server listening on port 6000!'));
