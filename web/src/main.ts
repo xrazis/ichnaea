5b7f6019de369e4761311e35c709d678fdf9bc2c
Haris Razis <dev@xrazis.com>
2020-12-20 21:15:50 +0200

    ➕ Add bulma and font-awesome

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 createApp(App).use(store).use(router).mount('#app')
