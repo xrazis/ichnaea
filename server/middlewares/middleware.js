e9fe93ea62bb66ed18707fe68bad27375e32b46d
Haris Razis <dev@xrazis.com>
2021-01-03 16:22:32 +0200

    🔁 Update dependencies & remove unused items

--- server/middlewares/middleware.js
+++ server/middlewares/middleware.js
@@ -1,21 +1,4 @@
-const {validationResult} = require('express-validator');
-
 module.exports = {
-    handleErrors(route) {
-        return async (req, res, next) => {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                req.session.errors = errors.errors;
-                return res.redirect('/auth/' + route);
-            }
-            req.session.errors = {};
-            next();
-        }
-    },
-    handleErrorsPassport(req, res, route, info) {
-        req.session.errors = info;
-        return res.redirect('/auth/' + route);
-    },
     requireAuth(req, res, next) {
         if (!req.isAuthenticated())
             return res.redirect('/auth/login');
