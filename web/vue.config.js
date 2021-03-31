a7782ae611cea485dae14883d2546df2ae4b340c
Haris Razis <dev@xrazis.com>
2021-03-31 13:37:07 +0300

    Change favicon and title

--- web/vue.config.js
+++ web/vue.config.js
@@ -7,5 +7,13 @@ module.exports = {
                 changeOrigin: true,
             }
         }
+    },
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'Ichnaea';
+                return args;
+            })
     }
 }
