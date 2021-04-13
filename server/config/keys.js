51e3d694f31da02d1eb5d3051d007a7b8fec4a65
Haris Razis <xrazis@open.gr>
2021-04-13 22:35:02 +0300

    Fix influx connection issue by defining global variables.

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,8 +1,9 @@
 module.exports = {
     session_secret: 'very secret this is',
     influx_url: 'http://influx:8086',
-    influx_org: 'xrazis',
-    influx_bucket: 'db0',
+    influx_org: 'razis',
+    influx_bucket: 'ichnaea',
+    influx_token: 'my-super-secret-auth-token',
     redis_uri: 'redis://redis:6379',
     mongo_uri: 'mongodb://root:password@mongo:27017/ichnaea?authSource=admin'
 }
--- server/connections/influx_conn.js
+++ server/connections/influx_conn.js
@@ -1,10 +1,11 @@
 const {InfluxDB} = require('@influxdata/influxdb-client')
 const chalk = require('chalk')
 
-const {influx_url, influx_org, influx_bucket} = require('../config/keys')
+const {influx_url, influx_org, influx_bucket, influx_token} = require('../config/keys')
 
 const client = new InfluxDB({
-    url: influx_url
+    url: influx_url,
+    token: influx_token
 });
 
 const writeApi = client.getWriteApi(influx_org, influx_bucket, 's');
