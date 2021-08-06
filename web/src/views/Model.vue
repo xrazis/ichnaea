7f65a61742249d87581b99433a0f568d1a02a026
Haris Razis <dev@xrazis.com>
2021-08-06 17:58:33 +0300

    Add complementary filter for acc and gyro

--- web/src/views/Model.vue
+++ web/src/views/Model.vue
@@ -1,24 +1,44 @@
 <template>
-  <h1 v-if="athlete" class="title is-2">/athletes/{{ athlete.name }}/chart</h1>
+  <h1 v-if="athlete" class="title is-2">/athletes/{{ athlete.name }}/model</h1>
 
-  <Renderer ref="renderer" antialias :orbit-ctrl="{ enableDamping: true, target }" resize shadow>
-    <Camera :position="{ x: 100, y: 200, z: 300 }"/>
-    <Scene ref="scene" background="#a0a0a0">
-      <HemisphereLight/>
+  <div v-if="athlete" class="notification is-primary is-light">
+    This is a realtime animation for "{{ athlete.name }}"!
+  </div>
 
-      <DirectionalLight
-          :position="{ x: 0, y: 200, z: 100 }"
-          cast-shadow :shadow-camera="{ top: 180, bottom: -120, left: -120, right: 120 }"
-      />
+  <div class="box">
+    <Renderer ref="renderer" :orbit-ctrl="{ enableDamping: true, target }" antialias resize shadow>
 
-      <Plane :width="2000" :height="2000" :rotation="{ x: -Math.PI / 2 }" receive-shadow>
-        <PhongMaterial color="#999999" :props="{ depthWrite: false }"/>
-      </Plane>
+      <Camera :position="{ x: 100, y: 200, z: 300 }"/>
 
-      <FbxModel src="Samba-Dancing.fbx" @load="onLoad" @error="onError"/>
-    </Scene>
-  </Renderer>
+      <Scene ref="scene" background="#a0a0a0">
+        <HemisphereLight/>
 
+        <DirectionalLight
+            :position="{ x: 0, y: 200, z: 100 }"
+            :shadow-camera="{ top: 180, bottom: -120, left: -120, right: 120 }" cast-shadow
+        />
+
+        <Plane :height="2000" :rotation="{ x: -Math.PI / 2 }" :width="2000" receive-shadow>
+
+          <PhongMaterial :props="{ depthWrite: false }" color="#999999"/>
+        </Plane>
+
+        <Box ref="box"
+             :position="{ x: 0, y: 50, z: 0 }"
+             :rotation="{ x: 0, y: 0, z: 0 }"
+             :scale="{ x: 100, y: 100, z: 100 }"
+             :cast-shadow="false"
+             :receive-shadow="false"
+        >
+          <BasicMaterial/>
+        </Box>
+
+
+        <!--        <FbxModel src="animation-model.fbx" @error="onError" @load="onLoad"/>-->
+      </Scene>
+
+    </Renderer>
+  </div>
 </template>
 
 <!--Only for this module the language is JS instead of TS.-->
@@ -28,19 +48,25 @@ import {defineComponent} from 'vue'
 import {AnimationMixer, Clock, Fog, GridHelper, Vector3} from 'three';
 import {
   AmbientLight,
+  BasicMaterial,
+  Box,
+  BoxGeometry,
   Camera,
   DirectionalLight,
   FbxModel,
   HemisphereLight,
-  Renderer,
   PhongMaterial,
   Plane,
+  Renderer,
   Scene,
 } from 'troisjs';
 
 export default defineComponent({
   components: {
     AmbientLight,
+    Box,
+    BoxGeometry,
+    BasicMaterial,
     Camera,
     DirectionalLight,
     FbxModel,
@@ -54,37 +80,82 @@ export default defineComponent({
     return {
       target: new Vector3(0, 100, 0),
       athlete: null,
+      temperature: 0,
+      gravitationalAcceleration: 9.82,
+      fractionAccuracy: 2,
+      samplingInterval: 1,
+      accelerometer: {},
+      gyroscope: {},
     };
   },
   mounted() {
     this.$store.dispatch('athlete_getOne', this.$route.params.id).then((res) => this.athlete = res.data);
+    this.$socket.client.on('console', this.parseData);
 
-    const scene = this.$refs.scene.scene;
-    scene.fog = new Fog(0xa0a0a0, 200, 1000);
-    const grid = new GridHelper(2000, 20, 0x000000, 0x000000);
-    grid.material.opacity = 0.5;
-    grid.material.transparent = true;
-    this.$refs.scene.add(grid);
+    this.initScene();
+  },
+  beforeUnmount() {
+    this.$socket.client.off('console', this.parseData);
   },
   methods: {
+    parseData(data) {
+      const {temperature, accelerometer, gyroscope} = data;
+
+      this.temperature = temperature;
+
+      // Get pitch, roll from gyro
+      let compPitch = gyroscope.pitch.angle;
+      let compRoll = gyroscope.roll.angle;
+
+      // Only use accelerometer when forces are ~1g
+      if (accelerometer.acceleration > 0.9 && accelerometer.acceleration < 1.1) {
+        compPitch = -compPitch * 0.95 + accelerometer.pitch * 0.05;
+        compRoll = compRoll * 0.95 + accelerometer.roll * 0.05;
+      }
+
+      console.log(`
+      pitch: ${compPitch}
+      inclination: ${accelerometer.inclination}
+      roll: ${compRoll}
+      `)
+
+      // this.$refs.box.mesh.rotation.x = compRoll;
+      // this.$refs.box.mesh.rotation.y = compPitch;
+
+    },
+    initScene() {
+      const scene = this.$refs.scene.scene;
+      scene.fog = new Fog(0xa0a0a0, 200, 1000);
+
+      const grid = new GridHelper(2000, 20, 0x000000, 0x000000);
+      grid.material.opacity = 0.5;
+      grid.material.transparent = true;
+      this.$refs.scene.add(grid);
+    },
     onLoad(object) {
       this.mixer = new AnimationMixer(object);
+
       const action = this.mixer.clipAction(object.animations[0]);
       action.play();
-      object.traverse(function (child) {
+
+      object.traverse(child => {
         if (child.isMesh) {
           child.castShadow = true;
           child.receiveShadow = true;
         }
       });
+
       this.clock = new Clock();
       this.$refs.renderer.onBeforeRender(this.updateMixer);
     },
     updateMixer() {
       this.mixer.update(this.clock.getDelta());
     },
+    animate() {
+
+    },
     onError(error) {
-      console.log('we got an error!', error)
+      console.log(error);
     }
   },
 });
