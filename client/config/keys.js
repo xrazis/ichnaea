b703759f8187413c752d6c9eaf94410174bdd419
Haris Razis <dev@xrazis.com>
2021-08-12 15:18:58 +0300

    Move id to /config

--- client/config/keys.js
+++ client/config/keys.js
@@ -1,3 +1,9 @@
+const {createHash} = require('crypto');
+const getMAC = require('getmac').default;
+const mac = getMAC();
+const id = createHash('md5').update(mac).digest('hex');
+
 module.exports = {
-    server_url: 'http://127.0.0.1:8000'
+    server_url: 'http://127.0.0.1:8000',
+    id: id,
 };
